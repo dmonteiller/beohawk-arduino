4b5ea6e0d4faceb585ddcba5551096759cd8d1ae
rjehangir <jehangir@usc.edu>
2011-03-22 02:17:09 +0000

    Added Arduino code.

---
+++ ART_Quadrotor/ART_Quadrotor.h
@@ -0,0 +1,92 @@
+
+#define GRAVITY 408 // 1g on the accelerometer
+#define BAUDRATE 115200
+#define MAGNETOMETER 0
+
+#define LEDYELLOW 36
+#define LEDRED 35
+#define LEDGREEN 37
+#define SWITCH1 41
+#define SWITCH2 40
+
+#define MINTHROTTLE 1200
+#define MIDCHANNEL 1500
+
+#define Gyro_Gain_X 0.4  //X axis Gyro gain
+#define Gyro_Gain_Y 0.41 //Y axis Gyro gain
+#define Gyro_Gain_Z 0.41 //Z axis Gyro gain
+
+// Define macros //
+#define ToRad(x) (x*0.01745329252)  // *pi/180
+#define ToDeg(x) (x*57.2957795131)  // *180/pi
+#define Gyro_Scaled_X(x) x*ToRad(Gyro_Gain_X) //Return the scaled ADC raw data of the gyro in radians for second
+#define Gyro_Scaled_Y(x) x*ToRad(Gyro_Gain_Y) //Return the scaled ADC raw data of the gyro in radians for second
+#define Gyro_Scaled_Z(x) x*ToRad(Gyro_Gain_Z) //Return the scaled ADC raw data of the gyro in radians for second
+
+// PID constants
+float Kproll = 3.0;
+float Kiroll = 0.0;
+float Kdroll = 1.0;
+float Kppitch = 3.0;
+float Kipitch = 0.0;
+float Kdpitch = 1.0;
+float Kpyaw = 2.0;
+float Kiyaw = 0.0;
+float Kdyaw = 0.0;
+
+// Define vars //
+float loopDt = 0.02; // This will be changed per loop
+long timer = 0;
+long telemetryTimer = 0;
+long compassReadTimer = 0;
+
+int motorsArmed = 0;
+
+float controlRoll = 0;
+float controlPitch = 0;
+float controlYaw = 0;
+
+float rollError;
+float rollErrorOld;
+float rollI;
+float rollD;
+float pitchError;
+float pitchErrorOld;
+float pitchI;
+float pitchD;
+float yawError;
+float yawErrorOld;
+float yawI;
+float yawD;
+
+// ADC storage
+int ADC_Ch[6];
+int ADC_Offset[6];
+
+int accelOffset[3] = {2073,2056,2010};
+int gyroOffset[3] = {1659,1618,1673};
+
+float Omega[3]= {0,0,0};
+
+// Transmitter Data Storage
+int RCInput[8];
+int pilotRoll = 0;
+int pilotRollOld = 0;
+int pilotPitch = 0;
+int pilotPitchOld = 0;
+int pilotYaw = 0;
+int pilotYawOld = 0;
+int pilotThrottle = 0;
+
+// DCM PID Values (these will be hard code initialized)
+float KpDCM_rollpitch = .002;
+float KiDCM_rollpitch = .0000005;
+float KpDCM_yaw = 1.5;
+float KiDCM_yaw = .00005;
+
+// Vehicle orientation angles
+float roll;
+float pitch;
+float yaw;
+
+int motor[4];
---
+++ ART_Quadrotor/SerialData.pde
@@ -0,0 +1,25 @@
+
+
+void sendTestData() {
+ Serial.print(controlRoll);Serial.print(", ");
+ Serial.print(controlPitch);Serial.print(", ");
+ Serial.print(controlYaw);Serial.print(",  "); 
+ Serial.print(motor[0]);Serial.print(", ");
+ Serial.print(motor[1]);Serial.print(", ");
+ Serial.print(motor[2]);Serial.print(", ");
+ Serial.print(motor[3]);Serial.print(", ");
+
+ Serial.print(ToDeg(roll)); Serial.print(", ");
+ Serial.print(ToDeg(pitch)); Serial.print(", ");
+ Serial.print(ToDeg(yaw)); Serial.print(", ");
+ 
+ Serial.print(readADCCorrected(0)); Serial.print(", ");
+ Serial.print(readADCCorrected(1)); Serial.print(", ");
+ Serial.print(readADCCorrected(2)); Serial.print(", ");
+ Serial.print(readADCCorrected(3)); Serial.print(", ");
+ Serial.print(readADCCorrected(4)); Serial.print(", ");
+ Serial.print(readADCCorrected(5)); Serial.print(", ");
+ 
+ Serial.println(" ");
+  
+}
